bb36d335bff434db2b4f136b8ba0f6b42ffbbea3
Vinicius Barbosa <94053437+Vinicius-Barbosa-de-Oliveira@users.noreply.github.com>
2024-03-12 17:39:25 -0300

    fazendo o botao

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+const meubotao = () => {
+    return (
+        <button>meu botao</button>
+    );
+}
+
 const App = () => {
     return (
       <div>
         <h1>Bem vindo ao mundo React</h1>
         <button>eu sou um botão html</button>
+
+        
+        <meubotao/>
+        <meubotao/>
+        <meubotao/>
       </div>
+
+
     );
   }
 
